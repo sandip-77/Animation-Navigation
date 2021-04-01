de4c3103cf7511ea36eca5df6f6039cc95d9610f
sandip-77 <74707260+sandip-77@users.noreply.github.com>
2021-04-01 17:33:56 +0530

    Add files via upload
    First commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+
+function toggleNav(){
+    //Toggle menu bars open/close
+    menuBars.classList.toggle('change');
+
+    //Toggling menu to active
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains("overlay-active")){
+        //Animate-In overlay
+        overlay.classList.remove('overlay-slide-left');
+        overlay.classList.add('overlay-slide-right');
+        //Animate In - nav items
+        nav1.classList.remove('slide-out-1');
+        nav1.classList.add('slide-in-1');
+        nav2.classList.remove('slide-out-2');
+        nav2.classList.add('slide-in-2');
+        nav3.classList.remove('slide-out-3');
+        nav3.classList.add('slide-in-3');
+        nav4.classList.remove('slide-out-4');
+        nav4.classList.add('slide-in-4');
+        nav5.classList.remove('slide-out-5');
+        nav5.classList.add('slide-in-5');
+    }else{
+        overlay.classList.remove('overlay-slide-right');
+        overlay.classList.add('overlay-slide-left');
+        //Animate out - nav items
+        nav1.classList.remove('slide-in-1');
+        nav1.classList.add('slide-out-1');
+        nav2.classList.remove('slide-in-2');
+        nav2.classList.add('slide-out-2');
+        nav3.classList.remove('slide-in-3');
+        nav3.classList.add('slide-out-3');
+        nav4.classList.remove('slide-in-4');
+        nav4.classList.add('slide-out-4');
+        nav5.classList.remove('slide-in-5');
+        nav5.classList.add('slide-out-5');
+    }
+};
+
+// Event listeners
+
+menuBars.addEventListener("click" , toggleNav);
+nav1.addEventListener('clcik', toggleNav);
+nav2.addEventListener('clcik', toggleNav);
+nav3.addEventListener('clcik', toggleNav);
+nav4.addEventListener('clcik', toggleNav);
+nav5.addEventListener('clcik', toggleNav);
+
